8aa2dab8a7fa408b7d64735b4d3d5220e175c9f6
echoWang <echoechowang@qq.com>
2018-04-13 22:07:32 +0800

    add unit tests

--- index.test.js
+++ index.test.js
@@ -1,18 +1,54 @@
 import log from './src/index'
 
-class Animal{
-    constructor(name){
-        this.name = name
-    }
-    @log.catchMethod
-    feed(food){
-        throw new Error('method error')
-    }
-}
-const cat = new Animal('cat')
-cat.feed('fish')
-
 test('log.catchMethod should catch and log the error in a method of class', () => {
+    class Animal{
+        constructor(name){
+            this.name = name
+        }
+        @log.catchMethod
+        feed(food){
+            throw new Error('method error')
+        }
+    }
+    const cat = new Animal('cat')
+    cat.feed('fish')
+
     const error = log.data.error.find(item=>item.message==='method error')
     expect(error).toBeTruthy()
+})
+
+
+test('log.catchClass should catch and log the error in methods of class', () => {
+    @log.catchClass
+    class Animal{
+        constructor(name){
+            this.name = name
+        }
+        feed(food){
+            throw new Error('method feed error')
+        }
+        jump(place){
+            throw new Error('method jump error')
+        }
+    }
+    const cat = new Animal('cat')
+    cat.feed('fish')
+    cat.jump('roof')
+    expect(log.data.error.map((item)=>item.message)).toContain('method feed error')
+    expect(log.data.error.map((item)=>item.message)).toContain('method jump error')
+})
+
+test('log.trackAction should log the action when induce', () => {
+    class Animal{
+        constructor(name){
+            this.name = name
+        }
+        @log.trackAction('feedCat')
+        feed(food){
+        }
+    }
+    const cat = new Animal('cat')
+    cat.feed('fish')
+
+    expect(log.data.action.map((item)=>item.message)).toContain('feedCat')
 })
